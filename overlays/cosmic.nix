60e69443b52c16758629f2699a96e2fc3f81b3e6
kleinbem <martin.kleinberger@gmail.com>
2025-12-08 00:04:59 +0000

    Update COSMIC xdg-desktop-portal hash

--- overlays/cosmic.nix
+++ overlays/cosmic.nix
@@ -1,8 +1,8 @@
+# overlays/cosmic.nix
 (final: prev: {
   # TEMP FIX 1: cosmic-edit hash mismatch
   cosmic-edit = prev.cosmic-edit.overrideAttrs (old: {
     src = old.src.override {
-      # "got" from the cosmic-edit error
       hash = "sha256-GN1Zts+v3ARcrkN+ZkMUSGNOAlIhXSYWRtWAyqUfUrY=";
     };
   });
@@ -10,17 +10,16 @@
   # TEMP FIX 2: cosmic-greeter hash mismatch
   cosmic-greeter = prev.cosmic-greeter.overrideAttrs (old: {
     src = old.src.override {
-      # "got" from the greeter/vendor error
       hash = "sha256-ERytoauws6FDJNXItflOE2MwjxwariiO8RXU1x1chkE=";
     };
   });
 
-  # TEMP FIX 3: xdg-desktop-portal-cosmic vendor hash mismatch
+  # TEMP FIX 3: xdg-desktop-portal-cosmic vendor/source hash mismatch
   xdg-desktop-portal-cosmic =
     prev.xdg-desktop-portal-cosmic.overrideAttrs (old: {
       src = old.src.override {
-        # same "got" hash as the failing ...-source.drv
-        hash = "sha256-ERytoauws6FDJNXItflOE2MwjxwariiO8RXU1x1chkE=";
+        # NEW "got" hash from latest error
+        hash = "sha256-7yfrjKHuYOWPMMkHdGZ+g0hynh2TtSf4h8zW13tTus4=";
       };
     });
 })
