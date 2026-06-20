555e99cb656ed4472e70f4acd7c8c1433821d642
kleinbem <martin.kleinberger@gmail.com>
2026-06-20 16:34:50 +0100

    feat(packages): expose container-{caddy,n8n,code-server} outputs
    Moves the container-derivation packages from the meta flake into nix-config
    itself, alongside the existing router-1-image. They were already deriving
    from self.nixosConfigurations.container-factory; now they live with their
    source.
    
    Part of the meta-flake → nix-config root migration (Phase 2/8).

--- modules/flake/images.nix
+++ modules/flake/images.nix
@@ -1,31 +1,45 @@
-{ inputs, ... }:
+{ self, inputs, ... }:
 {
   perSystem =
     { system, ... }:
     {
-      packages = {
-        router-1-image =
-          let
-            myInventory = import ../../inventory.nix;
-          in
-          inputs.nixos-generators.nixosGenerate {
-            inherit system;
-            modules = [
-              ../../hosts/router-1/default.nix
-              {
-                nixpkgs = {
-                  config.allowUnfree = true;
-                  buildPlatform = "x86_64-linux";
-                  hostPlatform = "aarch64-linux";
-                };
-              }
-            ];
-            format = "lxc";
-            specialArgs = {
-              inherit inputs myInventory;
-              inherit (inputs) self;
+      packages =
+        {
+          router-1-image =
+            let
+              myInventory = import ../../inventory.nix;
+            in
+            inputs.nixos-generators.nixosGenerate {
+              inherit system;
+              modules = [
+                ../../hosts/router-1/default.nix
+                {
+                  nixpkgs = {
+                    config.allowUnfree = true;
+                    buildPlatform = "x86_64-linux";
+                    hostPlatform = "aarch64-linux";
+                  };
+                }
+              ];
+              format = "lxc";
+              specialArgs = {
+                inherit inputs myInventory;
+                inherit (inputs) self;
+              };
             };
-          };
-      };
+        }
+        // (
+          if system == "x86_64-linux" then
+            let
+              containers = self.nixosConfigurations.container-factory.config.containers;
+            in
+            {
+              container-caddy = containers.caddy.path;
+              container-n8n = containers.n8n.path;
+              container-code-server = containers."code-server".path;
+            }
+          else
+            { }
+        );
     };
 }
