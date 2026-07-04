69d7227c64ece8dafb1edd8332f65985b4f7959e
kleinbem <martin.kleinberger@gmail.com>
2026-07-04 12:51:14 +0100

    Perfect unification of core-pi and hass-pi configurations

--- hosts/core-pi/default.nix
+++ hosts/core-pi/default.nix
@@ -4,7 +4,6 @@
   inputs,
   self,
   myInventory,
-  pkgs,
   ...
 }:
 {
@@ -113,8 +112,6 @@
       };
     };
   };
-
-
 
   # ─── Persistence ─────────────────────────────────────────────
   environment.persistence."/nix/persist" = {
